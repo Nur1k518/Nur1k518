bcceec324ff8ebb43fc5e5a2c38996baae4cec4a
Nur1k <abdullanrdaulet489@gmail.com>
2025-03-12 18:50:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmnurikdevshop import CPMNurikDevShop
+from ayandev import Ayandev
 
 __CHANNEL_USERNAME__ = "cpmnurik001"
 __GROUP_USERNAME__   = "Telegram-@cpmnurik2010"
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMNurikDevShop(acc_access_key)
+        cpm = Ayandev(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
